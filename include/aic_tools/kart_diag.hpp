389aae384b557543e4c04f6ab2cb660ba94f098b
Kenta Kato <kenta.kato.4321@gmail.com>
2024-11-04 02:49:11 +0900

    kart_diag の GnssFilter 継承を解消。各種アラートを追加。

--- include/aic_tools/kart_diag.hpp
+++ include/aic_tools/kart_diag.hpp
@@ -1,30 +1,61 @@
 #pragma once
 
-#include "aic_tools/gnss_filter.hpp"
-#include <rclcpp/time.hpp>
 #include <unordered_map>
+
+#include <rclcpp/rclcpp.hpp>
+#include <geometry_msgs/msg/pose_with_covariance_stamped.hpp>
+#include <std_msgs/msg/bool.hpp>
+#include <std_msgs/msg/empty.hpp>
 
 namespace aic_tools {
 
-class KartDiag : public GnssFilter{
+class KartDiag : public rclcpp::Node {
+
+public:
+  /* alias */
+  using PoseWithCovarianceStamped = geometry_msgs::msg::PoseWithCovarianceStamped;
+  using Bool = std_msgs::msg::Bool;
+  using Empty = std_msgs::msg::Empty;
 
 public:
   KartDiag();
 
 private:
-virtual void gnss_callback(const PoseWithCovarianceStamped::SharedPtr msg) override;
+
+// Subscribers
+rclcpp::Subscription<PoseWithCovarianceStamped>::SharedPtr sub_gnss_;
+rclcpp::Subscription<Bool>::SharedPtr sub_gnss_duplication_, sub_gnss_outlier_, sub_gnss_cov_large_, sub_ekf_cov_large_;
+rclcpp::Subscription<Empty>::SharedPtr sub_mpc_dead_signal_;
+
+// Publishers
+rclcpp::Publisher<Bool>::SharedPtr pub_gnss_data_lost_alert_;
+
+
+// Duplication detection
+size_t gnss_duplication_count_;
+std::unordered_map<size_t, size_t> gnss_duplication_count_map_;
+rclcpp::Time last_gnss_time_{0, 0};
+
+// Parameters
+size_t alert_gnss_duplication_count_;
+double alert_gnss_data_lost_sec_;
+
+// Timer
+rclcpp::TimerBase::SharedPtr timer_;
+void on_timer();
+
+void reset();
+
+void gnss_callback(const PoseWithCovarianceStamped::SharedPtr msg);
+void gnss_duplication_callback(const Bool::SharedPtr msg);
+void gnss_outlier_callback(const Bool::SharedPtr msg);
+void gnss_cov_large_callback(const Bool::SharedPtr msg);
+void ekf_cov_large_callback(const Bool::SharedPtr msg);
+void mpc_dead_signal_callback(const Empty::SharedPtr msg);
 void on_shutdown();
 void update_duplicate_count_map();
 void print_stats();
 
-// Duplication detection
-size_t gnss_duplicate_count_;
-std::unordered_map<size_t, size_t> gnss_duplicate_count_map_;
-rclcpp::Time last_gnss_time_{0, 0};
-
-// Parameters
-size_t alert_gnss_duplicate_count_;
-double alert_gnss_discontinuation_sec_;
 
 };
 
--- src/kart_diag.cpp
+++ src/kart_diag.cpp
@@ -1,69 +1,135 @@
 #include "aic_tools/kart_diag.hpp"
+#include <rclcpp/executors/multi_threaded_executor.hpp>
 #include <rclcpp/logging.hpp>
-#include <rclcpp/time.hpp>
 #include <rclcpp/utilities.hpp>
+#include <rclcpp/clock.hpp>
 
 namespace aic_tools {
 
-KartDiag::KartDiag() : GnssFilter("kart_diag") {
-  alert_gnss_duplicate_count_ =
-      static_cast<size_t>(declare_parameter<int64_t>("alert_gnss_duplicate_count"));
-  alert_gnss_discontinuation_sec_ =
-      declare_parameter<double>("alert_gnss_discontinuation_sec");
+KartDiag::KartDiag() : Node("kart_diag") {
+  alert_gnss_duplication_count_ =
+      static_cast<size_t>(declare_parameter<int64_t>("alert_gnss_duplication_count"));
+  alert_gnss_data_lost_sec_ =
+      declare_parameter<double>("alert_gnss_data_lost_sec");
+
+  // Subscribers
+  sub_gnss_ = this->create_subscription<PoseWithCovarianceStamped>(
+        "/sensing/gnss/pose_with_covariance",
+        rclcpp::QoS(rclcpp::KeepLast(10)).reliable(),
+        std::bind(&KartDiag::gnss_callback, this, std::placeholders::_1));
+  sub_gnss_duplication_ = this->create_subscription<Bool>(
+        "/gnss_filter/is_gnss_duplication_detected",
+        rclcpp::QoS(rclcpp::KeepLast(10)).best_effort(),
+        std::bind(&KartDiag::gnss_duplication_callback, this, std::placeholders::_1));
+  sub_gnss_outlier_ = this->create_subscription<Bool>(
+        "/gnss_filter/is_gnss_outlier_detected",
+        rclcpp::QoS(rclcpp::KeepLast(10)).best_effort(),
+        std::bind(&KartDiag::gnss_outlier_callback, this, std::placeholders::_1));
+  sub_gnss_cov_large_ = this->create_subscription<Bool>(
+        "/gnss_filter/is_gnss_cov_large",
+        rclcpp::QoS(rclcpp::KeepLast(10)).best_effort(),
+        std::bind(&KartDiag::gnss_cov_large_callback, this, std::placeholders::_1));
+  sub_ekf_cov_large_ = this->create_subscription<Bool>(
+        "/gnss_filter/is_ekf_cov_large",
+        rclcpp::QoS(rclcpp::KeepLast(10)).best_effort(),
+        std::bind(&KartDiag::ekf_cov_large_callback, this, std::placeholders::_1));
+  sub_mpc_dead_signal_ = this->create_subscription<Empty>(
+        "/mpc/dead_signal",
+        rclcpp::QoS(rclcpp::KeepLast(10)).best_effort(),
+        std::bind(&KartDiag::mpc_dead_signal_callback, this, std::placeholders::_1));
+
+  // Publishers
+  pub_gnss_data_lost_alert_ = this->create_publisher<Bool>(
+      "~/is_gnss_lost", rclcpp::QoS(rclcpp::KeepLast(10)).best_effort());
+
   rclcpp::on_shutdown([this]() { this->on_shutdown(); });
 
+  timer_ = this->create_wall_timer(
+      std::chrono::milliseconds(100),
+      std::bind(&KartDiag::on_timer, this));
+}
+
+void KartDiag::reset()
+{
+  gnss_duplication_count_ = 0;
+  gnss_duplication_count_map_.clear();
+  last_gnss_time_ = rclcpp::Time(0, 0);
 }
 
 void KartDiag::gnss_callback(const PoseWithCovarianceStamped::SharedPtr msg) {
-  if (last_gnss_time_.seconds() != 0) {
-    const auto diff_time = rclcpp::Time{msg->header.stamp} - last_gnss_time_;
-    if (diff_time.seconds() > alert_gnss_discontinuation_sec_) {
-      RCLCPP_WARN_STREAM(get_logger(), "GNSS time gap: " << diff_time.seconds() << " sec");
-    }
-  }
-  last_gnss_time_ = msg->header.stamp;
+  last_gnss_time_ = get_clock()->now();
+}
 
-  if (is_duplicate(*msg)) {
-    gnss_duplicate_count_++;
-    if (gnss_duplicate_count_ >= alert_gnss_duplicate_count_) {
-      RCLCPP_WARN_STREAM_THROTTLE(get_logger(), *get_clock(), 1000, "GNSS duplication count: " << gnss_duplicate_count_);
-    }
-  }
-  else{
+void KartDiag::gnss_duplication_callback(const Bool::SharedPtr msg) {
+  if (!msg->data) {
     update_duplicate_count_map();
-    gnss_duplicate_count_ = 0;
+    gnss_duplication_count_ = 0;
+    return;
   }
 
-  // push unique data to queue
-  if (gnss_queue_.size() >= gnss_queue_size_) {
-    gnss_queue_.pop_front();
+  gnss_duplication_count_++;
+  if (gnss_duplication_count_ >= alert_gnss_duplication_count_) {
+    RCLCPP_WARN_STREAM_THROTTLE(get_logger(), *get_clock(), 1000, "GNSS duplication count: " << gnss_duplication_count_);
   }
-  gnss_queue_.push_back(*msg);
+}
 
-  if (is_outlier(*msg))
-  {
-    RCLCPP_WARN(get_logger(), "GNSS outlier detected");
+void KartDiag::gnss_outlier_callback(const Bool::SharedPtr msg) {
+  if (msg->data) {
+    RCLCPP_WARN(get_logger(), "GNSS Outlier detected!");
   }
+}
 
+void KartDiag::gnss_cov_large_callback(const Bool::SharedPtr msg) {
+  if (msg->data) {
+    RCLCPP_WARN(get_logger(), "GNSS covariance is too large!");
+  }
+}
+
+void KartDiag::ekf_cov_large_callback(const Bool::SharedPtr msg) {
+  if (msg->data) {
+    RCLCPP_WARN(get_logger(), "EKF covariance is too large!");
+  }
+}
+
+void KartDiag::mpc_dead_signal_callback(const Empty::SharedPtr msg) {
+  RCLCPP_WARN(get_logger(), "MPC dead signal detected!");
+  print_stats();
+  reset();
+}
+
+void KartDiag::on_timer() {
+  Bool alert_msg;
+  if (last_gnss_time_.seconds() == 0.0) {
+    return;
+  }
+  const auto lost_duration = get_clock()->now() - last_gnss_time_;
+  if (lost_duration > rclcpp::Duration::from_seconds(alert_gnss_data_lost_sec_)) {
+    alert_msg.data = true;
+    RCLCPP_ERROR_STREAM_THROTTLE(get_logger(), *get_clock(), 500, "GNSS data lost for " << lost_duration.seconds() << " seconds");
+  }
+  else {
+    alert_msg.data = false;
+  }
+  pub_gnss_data_lost_alert_->publish(alert_msg);
 }
 
 void KartDiag::update_duplicate_count_map() {
-  if (gnss_duplicate_count_ > 0)
+  if (gnss_duplication_count_ > 0)
   {
-    if (gnss_duplicate_count_map_.find(gnss_duplicate_count_) == gnss_duplicate_count_map_.end())
+    if (gnss_duplication_count_map_.find(gnss_duplication_count_) == gnss_duplication_count_map_.end())
     {
-      gnss_duplicate_count_map_[gnss_duplicate_count_] = 1;
+      gnss_duplication_count_map_[gnss_duplication_count_] = 1;
     }
     else
     {
-      gnss_duplicate_count_map_[gnss_duplicate_count_]++;
+      gnss_duplication_count_map_[gnss_duplication_count_]++;
     }
   }
 }
 
 void KartDiag::print_stats() {
   // GNSS Value Duplication
-  std::map<size_t, size_t> sorted_duplicate_count_map(gnss_duplicate_count_map_.begin(), gnss_duplicate_count_map_.end());
+  std::map<size_t, size_t> sorted_duplicate_count_map(gnss_duplication_count_map_.begin(), gnss_duplication_count_map_.end());
   std::ostringstream oss;
   oss << "\n--------------------";
   oss << "\nGNSS duplication count: frequency";
@@ -81,7 +147,10 @@ void KartDiag::on_shutdown() {
 
 int main(int argc, char *argv[]) {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<aic_tools::KartDiag>());
+  const auto node = std::make_shared<aic_tools::KartDiag>();
+  auto exec = std::make_shared<rclcpp::executors::MultiThreadedExecutor>();
+  exec->add_node(node);
+  exec->spin();
   rclcpp::shutdown();
   return 0;
 }
